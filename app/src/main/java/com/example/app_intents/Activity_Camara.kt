5d565fff475e1c04684947e547c87a075b8ed88b
Diego Hernandez <hcdiego2012@gmail.com>
2025-02-15 19:06:51 -0600

    se agrego el activity de camara y se implementó la funcionalidad de la camara

--- app/src/main/java/com/example/app_intents/Activity_Camara.kt
+++ app/src/main/java/com/example/app_intents/Activity_Camara.kt
@@ -1,20 +1,63 @@
 package com.example.app_intents
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+
 
 class Activity_Camara : AppCompatActivity() {
+    private lateinit var imagen: ImageView
+    private lateinit var camara_btn: Button
+
+    companion object {
+        private const val REQUEST_IMAGE_CAPTURE = 1
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_camara)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        imagen = findViewById(R.id.imagen_camara)
+        camara_btn = findViewById(R.id.button_camara)
+
+        // Verificar y solicitar permisos
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+            != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 100)
+        }
+
+        camara_btn.setOnClickListener {
+            openCamera()
+        }
+    }
+    private fun openCamera() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (takePictureIntent.resolveActivity(packageManager) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        } else {
+            println("⚠️ No hay aplicación de cámara disponible")
+        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            imagen.setImageBitmap(imageBitmap)
         }
     }
 }
