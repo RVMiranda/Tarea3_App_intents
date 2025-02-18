819336c70274c1d58f1fabba0e5f01435467010e
Eduardo860 <ep321889@gmail.com>
2025-02-17 18:42:23 -0600

    Segunda Prueba

--- app/src/main/java/com/example/app_intents/SMS.kt
+++ app/src/main/java/com/example/app_intents/SMS.kt
@@ -1,6 +1,7 @@
 package com.example.app_intents
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +10,13 @@ import androidx.core.view.WindowInsetsCompat
 class SMS : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_sms)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // Obtener los datos del intent
+        val sender = intent.getStringExtra("sms_sender")
+        val messageBody = intent.getStringExtra("sms_body")
+
+        // Aquí podrías mostrar el mensaje en tu UI
+        Log.d("SMS", "Sender: $sender, Message: $messageBody")
     }
 }
